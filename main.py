f1b4d8fd13fac820fda5d394e8c7141dd15b3262
claudio.cordoba <Peya2021>
2021-09-08 16:00:16 -0300

    se comitea commit inicial

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
-
+#httplib lo uso para hacer las peticiones
 import httplib2
+#os lo uso para obtener la ruta actual
 import os
+#sys lo uso para obtener variables de sistema
 import sys
+#enviowpp lo uso para enviar los wpp por medio de navegar (para no pagar una api)
 import EnvioWpp as ew
+#apiclient es la api de google para acceder a los clients
 import apiclient as ap
+#oauth2 es para autentificarme oauth2.(para iniciar session en youtube)
+#esto me crea el main.py-oauth2.json y ahi tengo mi cuenta de youtube, donde tengo permisos para el youtube de comunidad cristiana
+#borrando este archivo. te vas a podes loguear en cualquier otro
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 import datetime
+
+
 def fecha():
+  #Proceso que calcula la fecha a levantar
   today = datetime.date.today()
   miercoles = today + datetime.timedelta( (2-today.weekday()) % 7 )
   domingo = today + datetime.timedelta( (6-today.weekday()) % 7 )
@@ -25,24 +35,15 @@ def fecha():
       StartscheduleTime='2021-'+str(domingo.month).zfill(2)+'-'+str(domingo.day)+'T13:30:00.000Z'
       finishscheduleTime = '2021-' + str(domingo.month).zfill(2) + '-' + str(domingo.day) + 'T16:30:00.000Z'
       return domingo,titulo,file,StartscheduleTime,finishscheduleTime,"Domingo"
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the {{ Google Cloud Console }} at
-# {{ https://cloud.google.com/console }}.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "client_secret.json"
 
+
+########################################################################################################## OAUTH2
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account.
+CLIENT_SECRETS_FILE = "client_secret.json"
 YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-
 # This variable defines a message to display if the CLIENT_SECRETS_FILE is
 # missing.
 MISSING_CLIENT_SECRETS_MESSAGE = """
@@ -57,6 +58,7 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
 
+
 def get_authenticated_service(args):
   flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
     scope=YOUTUBE_READ_WRITE_SCOPE,
@@ -70,10 +72,12 @@ def get_authenticated_service(args):
 
   return ap.discovery.build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     http=credentials.authorize(httplib2.Http()))
+##########################################################################################################
 
 # Create a liveBroadcast resource and set its title, scheduled start time,
 # scheduled end time, and privacy status.
 def insert_broadcast(youtube, options):
+  #creo el link
   insert_broadcast_response = youtube.liveBroadcasts().insert(
     part="snippet,status",
     body=dict(
@@ -91,7 +95,6 @@ def insert_broadcast(youtube, options):
   ).execute()
 
   snippet = insert_broadcast_response["snippet"]
-
   print ("Broadcast '%s' with title '%s' was published at '%s'." % (
     insert_broadcast_response["id"], snippet["title"], snippet["publishedAt"]))
   return insert_broadcast_response["id"]
@@ -100,46 +103,15 @@ def insert_broadcast(youtube, options):
 # This resource describes the content that you are transmitting to YouTube.
 
 def upload_thumbnail(youtube, video_id, file):
-
+#subo la imagen del dia
   youtube.thumbnails().set(
     videoId=video_id,
     media_body=file
   ).execute()
 
-def insert_stream(youtube, options):
-  insert_stream_response = youtube.liveStreams().insert(
-    part="snippet,cdn",
-    body=dict(
-      snippet=dict(
-        title=options.stream_title
-      ),
-      cdn=dict(
-        format="1080p",
-        ingestionType="rtmp"
-      )
-    )
-  ).execute()
-
-  snippet = insert_stream_response["snippet"]
-
-  print ("Stream '%s' with title '%s' was inserted." % (
-    insert_stream_response["id"], snippet["title"]))
-  return insert_stream_response["id"]
-
-# Bind the broadcast to the video stream. By doing so, you link the video that
-# you will transmit to YouTube to the broadcast that the video is for.
-def bind_broadcast(youtube, broadcast_id, stream_id):
-  bind_broadcast_response = youtube.liveBroadcasts().bind(
-    part="id,contentDetails",
-    id=broadcast_id,
-    streamId=stream_id
-  ).execute()
-
-  print ("Broadcast '%s' was bound to stream '%s'." % (
-    bind_broadcast_response["id"],
-    bind_broadcast_response["contentDetails"]["boundStreamId"]))
 
 if __name__ == "__main__":
+  #calculo fecha,titulo,inicio y fin de reunion segn dia a ejecutar
   fecha,titulo,file,StartscheduleTime,finishscheduleTime,dia=fecha()
   argparser.add_argument("--broadcast-title", help="Broadcast title",
     default=titulo)
@@ -152,13 +124,16 @@ if __name__ == "__main__":
   argparser.add_argument("--stream-title", help="Stream title",
     default=titulo)
   args = argparser.parse_args()
-
+  # mme autentifico con youtube
   youtube = get_authenticated_service(args)
 
   try:
+    #creo el link
     broadcast_id = insert_broadcast(youtube, args)
+    #actualizo la imagen
     upload_thumbnail(youtube, broadcast_id, file)
     if broadcast_id != '':
+      #envio el wpp con selenium
       ew.envioWpp(dia,"https://www.youtube.com/watch?v="+broadcast_id)
   except (ap.http.HttpError):
     print ("An HTTP error %d occurred:\n")
